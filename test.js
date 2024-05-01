0ae3ef92290ead9d0e40205cc85da8eda49a4e11
Muhaiminul Islam <raju.ridmik@gmail.com>
2024-05-01 10:28:43 +0600

    updated code

--- test.js
+++ test.js
@@ -12,9 +12,41 @@ function generateRandomEmail() {
     const domains = ["gmail.com", "yahoo.com", "hotmail.com", "outlook.com", "aol.com"];
     const randomDomain = domains[Math.floor(Math.random() * domains.length)];
 
-    const randomName = generateRandomName().replace(/\s/g, '').toLowerCase(); // Remove spaces and convert to lowercase
+    const randomName = generateRandomName().replace(/\s/g, '').toLowerCase();
     const randomNumber = Math.floor(Math.random() * 1000); // Random number for uniqueness
 
     return `${randomName}${randomNumber}@${randomDomain}`;
 }
 
+
+function createRandomContactNumber() {
+    const dialingCodes = ["02", "03", "07", "08"];
+    const randomDialingCode = dialingCodes[Math.floor(Math.random() * dialingCodes.length)];
+    const randomDigit = Math.floor(10000000 + Math.random() * 90000000);
+
+    return `+61${randomDialingCode}${randomDigit}`;
+}
+
+function checkCheckboxesRandomly() {
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach(checkbox => {
+        checkbox.checked = Math.random() >= 0.5;
+    });
+}
+
+function processFormSubmission() {
+    document.getElementById('mat-input-7').value = generateRandomName();
+    document.getElementById('mat-input-8').value = generateRandomEmail();
+    document.getElementById('mat-input-9').value = createRandomContactNumber();
+
+
+}
+
+function performMultipleSubmissions() {
+    for (let i = 0; i < 20; i++) {
+        setTimeout(processFormSubmission, i * 6000);
+    }
+}
+
+document.querySelector('.mat-button-wrapper').click();
+setTimeout(performMultipleSubmissions, 3000);
